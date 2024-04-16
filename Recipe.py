9f45e3e518d9ebc9fd0efff0669b591321faf921
Younghoon Cho <younghooncho@Younghoons-MacBook-Pro.local>
2024-04-16 16:49:40 -0400

    all class and methods. Return 0 to avoid red lines.

---
+++ Recipe.py
@@ -0,0 +1,32 @@
+# get the name of the recipe
+def get_name():
+
+    return 0
+
+# returns the cookTime for a recipe in HH:MM format with leading zeros
+def get_cook_time():
+
+    return 0
+
+# returns the prepTime for a recipe in HH:MM format with leading zeros
+def get_prep_time():
+
+    return 0
+
+
+# returns the recipeYield for a given recipe
+def get_recipe_yield():
+
+    return 0
+
+#downloads an image from the web and displays it in the UI. An ASCII-based progress-bar must be
+#displayed in the command line as images are downloaded. This should include the index of the image downloaded so
+#far. (Example: ! Downloading image 10 of xxx . . . ) <- look at PDF
+def set_image(url):
+    
+    return 0
+
+# returns the name of an image file for saving or displaying in the UI
+def get_image():
+
+    return 0
---
+++ RecipeUI.py
@@ -0,0 +1,30 @@
+#initalizes variables such as window height, width, and calls setup_window() to set up the window
+def __init__():
+    return 0
+
+# sets up the window and assigns a window title
+def setup_window():
+    return 0
+
+# takes in a list of recipe objects and displays them using a GridLayout as shown in the image -> more info at PDF
+def layout_ui(recipes):
+    return 0
+
+# called when the user clicks the next button. 
+def next():
+    return 0
+
+def previous():
+    return 0
+
+def first():
+    return 0
+
+def last():
+
+def search(recipe_keywords):
+    return 0
+
+# clears the search results and returns to normal pagination of the list of recipes
+def reset():
+    return 0
