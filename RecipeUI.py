693ea667350c41a81a276abf2e730465a1bea80b
Younghoon Cho <younghooncho@Younghoons-MacBook-Pro.local>
2024-04-18 13:55:31 -0400

    code runs for RecipeUI

--- RecipeUI.py
+++ RecipeUI.py
@@ -1,33 +1,44 @@
+import sys
 from PyQt5.QtWidgets import  *
+from PyQt5.QtGui import QIcon
 
-#initalizes variables such as window height, width, and calls setup_window() to set up the window
-def __init__():
-    return 0
+class RecipeUI(QMainWindow):
+    #initalizes variables such as window height, width, and calls setup_window() to set up the window
+    def __init__(self):
+        super(RecipeUI, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("Example Window")
+        # note that some systems like macOS will not show the icon on the window
+        self.setWindowIcon(QIcon('my_ui_icon.png'))
+        self.window()
 
-# sets up the window and assigns a window title
-def setup_window():
-    return 0
+    # sets up the window and assigns a window title
+    def setup_window():
+        
+        return 0
 
-# takes in a list of recipe objects and displays them using a GridLayout as shown in the image -> more info at PDF
-def layout_ui(recipes):
-    return 0
+    # takes in a list of recipe objects and displays them using a GridLayout as shown in the image -> more info at PDF
+    def layout_ui(recipes):
+        return 0
 
-# called when the user clicks the next button. 
-def next():
-    return 0
+    # called when the user clicks the next button. 
+    def next():
+        return 0
 
-def previous():
-    return 0
+    def previous():
+        return 0
 
-def first():
-    return 0
+    def first():
+        return 0
 
-def last():
-    return 0
+    def last():
+        return 0
 
-def search(recipe_keywords):
-    return 0
+    def search(recipe_keywords):
+        return 0
 
-# clears the search results and returns to normal pagination of the list of recipes
-def reset():
-    return 0
+    # clears the search results and returns to normal pagination of the list of recipes
+    def reset():
+        return 0
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import  *
+from PyQt5.QtGui import QIcon
+
+from RecipeUI import RecipeUI
+
+def main():
+    app = QApplication(sys.argv)
+    gui = RecipeUI()
+    gui.show()
+    sys.exit(app.exec_())
+main()
