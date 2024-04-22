e10c3dc45d92277cc187d54a722ede137c7433cc
Younghoon Cho <younghooncho@Younghoons-MacBook-Pro.local>
2024-04-22 05:35:10 -0400

    view_recipe_button should be fixed

--- RecipeUI.py
+++ RecipeUI.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt
 
 from Recipe import Recipe
 from RecipeProcessor import RecipeProcessor
-
+from RecipeDetails import RecipeDetails
 recipes = RecipeProcessor()
 recipes.load_recipes("recipes.json")
 
@@ -70,14 +70,12 @@ class RecipeUI(QMainWindow,QWidget):
         previous_button.clicked.connect(self.previous)
         next_button.clicked.connect(self.next)
         last_button.clicked.connect(self.last)
-
         # Combine all layouts into the main layout
         main_layout.addLayout(search_layout)
         main_layout.addLayout(self.recipe_grid)
         main_layout.addLayout(navigation_layout)
 
     def layout_ui(self, recipes):
-        # Assume the list of recipes comes from somewhere else in your application
         
         recipes = recipes.get_recipes()
         self.max_num = len(recipes)
@@ -85,27 +83,51 @@ class RecipeUI(QMainWindow,QWidget):
         grid_layout = QGridLayout()
 
         # Create and add recipe cards to the grid layout
-        for i in range(4*self.current_page,4*self.current_page+4):
-            if i+4*self.current_page > self.max_num:
-                break
-            recipe_card = QLabel()
-            recipe_card.setText(recipes[i].get_name())
-            grid_layout.addWidget(recipe_card, i // 2, i % 2)  # 2 columns grid
+        for i in range(4*self.current_page-1,4*self.current_page+3):
+            if i > self.max_num:
+                return grid_layout
+            else:
+
+
+                recipe_widget = QWidget()
+                recipe_layout = QVBoxLayout(recipe_widget)
+
+                image_label = QLabel()
+                pixmap = QPixmap(recipes[i].get_image()) 
+                image_label.setPixmap(pixmap.scaled(200, 180, Qt.KeepAspectRatio))
+                recipe_layout.addWidget(image_label)
+        
+                recipe_number_label = QLabel(f'Recipe #: {i+2}')
+                recipe_name_label = QLabel(f'Recipe Name: {recipes[i].get_name()}')  # Update with actual recipe name
+                prep_time_label = QLabel(f'Prep Time: {recipes[i].get_prep_time()}')  # Update with actual prep time
+                cook_time_label = QLabel(f'Cook Time: {recipes[i].get_cook_time()}')  # Update with actual cook time
+                view_recipe_button = QPushButton('View Recipe')
+                # Optional: Connect view_recipe_button to a function to handle button click
+                
+                # Add labels and button to the recipe layout
+                recipe_layout.addWidget(recipe_number_label)
+                recipe_layout.addWidget(recipe_name_label)
+                recipe_layout.addWidget(prep_time_label)
+                recipe_layout.addWidget(cook_time_label)
+                recipe_layout.addWidget(view_recipe_button)
+                
+                # view_recipe_button.clicked.connect(self.show_recipe(i))
+
+                grid_layout.addWidget(recipe_widget, (i+1) // 2, (i+1) % 2)  # 2 columns grid
 
         # Set the grid layout to the main window
         self.setLayout(grid_layout)
 
         return grid_layout
 
+    
     def next(self):
         
         if self.current_page == self.max_num:
-            pass
+            return
         else:
             self.current_page = self.current_page + 1
             self.setup_window()
-        
-
        
 
     def previous(self):
@@ -130,6 +152,11 @@ class RecipeUI(QMainWindow,QWidget):
         self.current_page = max_page
         self.setup_window()
 
+    def show_recipe(self, index: int):
+        details = RecipeDetails()
+        details.setupUi(details)
+        details.set_recipe(recipes.get_recipes()[index], index + 1)
+        details.exec()
 
     def search(self, recipe_keywords):
         
